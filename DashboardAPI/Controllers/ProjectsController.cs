787c5244aac57f4c85231b9658e5a4e10431f9a8
Sambela Siva Sai <QD107@cortlandglobal.com>
2024-06-11 18:18:24 +0530

    first commit

---
+++ DashboardAPI/Controllers/ProjectsController.cs
@@ -0,0 +1,29 @@
+﻿using DashboardAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DashboardAPI.Controllers
+{
+    public class ProjectsController : Controller
+    {
+
+        [HttpGet]
+        [Route("api/projects")]
+        public List<Project> Get()
+        {
+            TaskManagerDbContext dbContext = new TaskManagerDbContext();
+            List<Project> projects = dbContext.Projects.ToList();
+            return projects;
+            //return View();
+        }
+
+        [HttpPost]
+        [Route("api/projects")]
+        public Project Post(Project p)
+        {
+            TaskManagerDbContext dbContext = new TaskManagerDbContext();
+            dbContext.Projects.Add(p);
+            dbContext.SaveChanges();
+            return p;
+        }
+    }
+}
---
+++ DashboardAPI/Models/Project.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DashboardAPI.Models
+{
+    public class Project
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+
+        [NotMapped]
+       // [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DateOfStart { get; set; }
+
+        public int TeamSize { get; set; }
+
+    }
+
+    public class TaskManagerDbContext: DbContext
+    {
+        public DbSet<Project>  Projects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("data source=(localdb)\\MSSQLLocalDB;integrated security=yes;initial catalog=TaskManager;User Id=CORTLAND\\QD107;Password=Welcome2AD!;");
+        }
+    }
+}
